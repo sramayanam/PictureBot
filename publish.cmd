f70fff2f4e82276801cd5c170e2e75c259dabbc7
Sree Ram <sreekanth_bhr@yahoo.com>
2019-02-17 14:47:49 -0600

    Initial Commit Picture Bot V4 Frame work Dotnet Core

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild BasicBot.sln -p:DeployOnBuild=true -p:PublishProfile=srrampicturebot-Web-Deploy.pubxml -p:Password=vMYph0llnd21WMpkxmTk470AAmjzSe0uP9iMrSm60ibr3qi8xxc0XA2qAgx5
+
